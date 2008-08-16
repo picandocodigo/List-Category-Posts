9d7f34ffb4c7673aa2043aec7b36e9fb501b2ea2
Fernando Briano <fernando@picandocodigo.net>
2008-08-16 20:27:29 +0000

    Adding version 0.1 of the plugin
    git-svn-id: http://plugins.svn.wordpress.org/list-category-posts/trunk@59980 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ list_cat_posts.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: List category posts.
+Plugin URI: http://picandocodigo.net/programacion/wordpress/list-category-posts-wordpress-plugin-english/
+Description: List some posts from a category into a post/page using [catlist=ID]. Inspired by Category Page: http://wordpress.org/extend/plugins/page2cat/ 
+Version: 0.1
+Author: Fernando Briano
+Author URI: http://picandocodigo.net/wordpress/
+*/
+
+/* Copyright 2008  Fernando Briano  (email : transformers.es@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or 
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*The following code is based on code from: Category Page: http://wordpress.org/extend/plugins/page2cat/
+ * Pixline - http://pixline.net/ */
+// filter the content of a page, check for tag and replace it with a list of posts in the requested category.
+// function heavily inspired from Alex Rabe NextGen Gallery's nggfunctions.php. 
+function list_category_posts($content){
+global $post;
+	if ( stristr( $content, '[catlist' )) {
+		$search = "@(?:<p>)*\s*\[catlist\s*=\s*(\w+|^\+)\]\s*(?:</p>)*@i";
+		if	(preg_match_all($search, $content, $matches)) {
+			if (is_array($matches)) {
+				$limit = get_option('lcp_limit');		
+				foreach ($matches[1] as $key =>$v0) {
+					$output = "<ul class='lcp_catlist'>";
+					$catposts = get_posts('category='.$v0."&numberposts=".$limit);
+					foreach($catposts as $single):
+						$output .= "<li><a href='".get_permalink($single->ID)."'>".$single->post_title."</a></li>";
+					endforeach;
+					$search = $matches[0][$key];
+					$output .= "</ul>";
+					$replace= $output;
+					$content= str_replace ($search, $replace, $content);
+				}
+			}
+		}
+	}
+return $content;
+}
+
+function lcp_add_option_page(){
+	add_options_page('Category List', 'Category List', 'manage_options','listcat/list_cat_posts_options.php');
+}
+
+add_filter('the_content','list_category_posts');
+add_action('admin_head', 'lcp_add_option_page');
+?>
