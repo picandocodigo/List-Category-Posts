eb244f3e8488caaa1305d1b1e97fac175760a234
Fernando Briano <fernando@picandocodigo.net>
2008-12-30 01:53:18 +0000

    Added new version 0.4, with lots of changes including WP 2.7 testing.
    git-svn-id: http://plugins.svn.wordpress.org/list-category-posts/trunk@82951 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lcp_widget_form.php
+++ lcp_widget_form.php
@@ -1,53 +1,59 @@
 <?php
-	$options = get_option("lcp_widget_options");
-	if (!is_array( $options)){
-		$options = array(
-		'lcp_widget_categoryid' => '1',
-		'lcp_widget_limit' => '5',
-		'lcp_widget_orderby' => 'date',
-		'lcp_widget_order' => 'asc'
-		);	
-	}
+$options = get_option("lcp_widget_options");
+if (!is_array( $options)){
+  $options = array(
+		   'lcp_widget_title' => 'Write a title',
+		   'lcp_widget_categoryid' => '1',
+		   'lcp_widget_limit' => '5',
+		   'lcp_widget_orderby' => 'date',
+		   'lcp_widget_order' => 'asc'
+		   );	
+}
 ?>
+<p>Title:<br/>
+<input type='text' name='lcp_widget_title'></p>
 <p>Category:<br/>
 <select name="lcp_widget_categoryid">
-	<?php 
-		$categories=  get_categories(); 
-		foreach ($categories as $cat) {
-				$option = '<option value="'.$cat->cat_ID.'">';
-				$option .= $cat->cat_name;
-				$option .= '</option>';
-				echo $option;
-		}
-	?>
+  <?php 
+  $categories=  get_categories(); 
+foreach ($categories as $cat) {
+  $option = '<option value="'.$cat->cat_ID.'">';
+  $option .= $cat->cat_name;
+  $option .= '</option>';
+  echo $option;
+}
+?>
 </select></p>
 
 <p>Number of posts:<br/>
 <input type='text' name='lcp_widget_limit'></p>
 <p>Order By:<br/>
 <select name='lcp_widget_orderby'>
-	<option value='date'>Date</option>
-	<option value='title'>Post title</option>
-	<option value='author'>Author</option>
-	<option value='rand'>Random</option>
+  <option value='date'>Date</option>
+  <option value='title'>Post title</option>
+  <option value='author'>Author</option>
+  <option value='rand'>Random</option>
 </select></p>
 <p>Order:<br/>
 <select name='lcp_widget_order'>
-	<option value='desc'>Descending</option>
-	<option value='asc'>Ascending</option>
+  <option value='desc'>Descending</option>
+  <option value='asc'>Ascending</option>
 </select></p>
 
 <?php
 if ($_POST['lcp_widget_submit']){
-	$options['lcp_widget_categoryid']=htmlspecialchars($_POST['lcp_widget_categoryid']);
-	$options['lcp_widget_limit']=htmlspecialchars($_POST['lcp_widget_limit']);
-	$options['lcp_widget_orderby']=htmlspecialchars($_POST['lcp_widget_orderby']);
-	$options['lcp_widget_order']=htmlspecialchars($_POST['lcp_widget_order']);
+  
+  $options['lcp_widget_title']=htmlspecialchars($_POST['lcp_widget_title']);
+  $options['lcp_widget_categoryid']=htmlspecialchars($_POST['lcp_widget_categoryid']);
+  $options['lcp_widget_limit']=htmlspecialchars($_POST['lcp_widget_limit']);
+  $options['lcp_widget_orderby']=htmlspecialchars($_POST['lcp_widget_orderby']);
+  $options['lcp_widget_order']=htmlspecialchars($_POST['lcp_widget_order']);
 
-	update_option("lcp_widget_categoryid", $options['lcp_widget_categoryid']);
-	update_option("lcp_widget_limit", $options['lcp_widget_limit']);
-	update_option("lcp_widget_orderby", $options['lcp_widget_orderby']);
-	update_option("lcp_widget_order", $options['lcp_widget_order']);
+  update_option("lcp_widget_title", $options['lcp_widget_title']);
+  update_option("lcp_widget_categoryid", $options['lcp_widget_categoryid']);
+  update_option("lcp_widget_limit", $options['lcp_widget_limit']);
+  update_option("lcp_widget_orderby", $options['lcp_widget_orderby']);
+  update_option("lcp_widget_order", $options['lcp_widget_order']);
 }
 ?>
 	<input type="hidden" id="lcp_widget_submit" name="lcp_widget_submit" value="1" />
