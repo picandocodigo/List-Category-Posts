96412a657b328b6526096803eb28d5f916b638d4
fernandobt <fernandobt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-25 16:00:06 +0000

    Small fix for 0.19.2
    git-svn-id: http://plugins.svn.wordpress.org/list-category-posts/trunk@455545 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- list_cat_posts.php
+++ list_cat_posts.php
@@ -3,7 +3,7 @@
 Plugin Name: List category posts
 Plugin URI: http://picandocodigo.net/programacion/wordpress/list-category-posts-wordpress-plugin-english/
 Description: List Category Posts allows you to list posts from a category into a post/page using the [catlist] shortcode. This shortcode accepts a category name or id, the order in which you want the posts to display, and the number of posts to display. You can use [catlist] as many times as needed with different arguments. Usage: [catlist argument1=value1 argument2=value2].
-Version: 0.19.1
+Version: 0.19.2
 Author: Fernando Briano
 Author URI: http://picandocodigo.net/
 */
@@ -60,10 +60,10 @@ class ListCategoryPosts{
                             'class' => 'lcp_catlist',
                             'customfield_name' => '',
                             'customfield_value' =>'',
-                            'customfield_display' =>''
+                            'customfield_display' =>'',
+                            'taxonomy' => ''
                     ), $atts);
 
-            
             $catlist_displayer = new CatListDisplayer($atts);
             return $catlist_displayer->display();
 
@@ -82,6 +82,7 @@ Include the latest post of a specific category on a matching page
   http://wordpress.stackexchange.com/questions/26224/how-to-include-the-latest-post-of-a-specific-category-on-a-matching-page
 Getting the “more” tag to work with plugin-list-category-post
   http://wordpress.stackexchange.com/questions/30376/getting-the-more-tag-to-work-with-plugin-list-category-post
+- Fix the code for the WordPress Coding Standards: http://codex.wordpress.org/WordPress_Coding_Standards
 - i18n
 - Pagination
 - Simpler template system
