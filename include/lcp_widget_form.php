e52ba5afd0f15a0304273fc326d36abf26500757
fernandobt <fernandobt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-07 17:12:23 +0000

    Added a feature, fixed readme.txt
    git-svn-id: http://plugins.svn.wordpress.org/list-category-posts/trunk@460291 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/lcp_widget_form.php
+++ include/lcp_widget_form.php
@@ -15,98 +15,98 @@ $instance = wp_parse_args( (array) $instance, array(
                 'show_excerpt'=>'',
                 'exclude'=>'',
                 'excludeposts'=>'',
-		'thumbnail' =>'',
+                'thumbnail' =>'',
                 'offset'=>'',
                 'show_catlink'=>'' ) );
 $title = strip_tags($instance['title']);
 $limit = strip_tags($instance['limit']);
 $orderby = strip_tags($instance['orderby']);
 $order = strip_tags($instance['order']);
-$showdate = strip_tags($instance['showdate']);
-$showauthor = strip_tags($instance['author']);
+$showdate = strip_tags($instance['show_date']);
+$showauthor = strip_tags($instance['show_author']);
 $exclude = strip_tags($instance['exclude']);
 $excludeposts = strip_tags($instance['excludeposts']);
 $offset = strip_tags($instance['offset']);
-$showcatlink = strip_tags($instance['catlink']);
+$showcatlink = strip_tags($instance['show_catlink']);
 $categoryid = strip_tags($instance['categoryid']);
-$showexcerpt = strip_tags($instance['excerpt']);
+$showexcerpt = strip_tags($instance['show_excerpt']);
 $thumbnail = strip_tags($instance['thumbnail']);
 $thumbnail_size = strip_tags($instance['thumbnail_size']);
 ?>
 
-<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e("Title")?></label>
+<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e("Title", 'list-category-posts')?></label>
 <br/>
-	<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" 
-	name="<?php echo $this->get_field_name('title'); ?>" type="text" 
-	value="<?php echo attribute_escape($title); ?>" />
+        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" 
+        name="<?php echo $this->get_field_name('title'); ?>" type="text" 
+        value="<?php echo esc_attr($title); ?>" />
 </p>
 
-<p><label for="<?php echo $this->get_field_id('categoryid'); ?>"><?php _e("Category")?></label>
+<p><label for="<?php echo $this->get_field_id('categoryid'); ?>"><?php _e("Category", 'list-category-posts')?></label>
 <br/>
 <select id="<?php echo $this->get_field_id('categoryid'); ?>" name="<?php echo $this->get_field_name('categoryid'); ?>">
-	<?php 
-		$categories=  get_categories();
-		foreach ($categories as $cat) :
-			$option = '<option value="' . $cat->cat_ID . '" ';
-			if ($cat->cat_ID == $categoryid) :
-				$option .= ' selected = "selected" ';
-			endif;
-			$option .=  '">';
-			$option .= $cat->cat_name;
-			$option .= '</option>';
-			echo $option;
-		endforeach;
-	?>
+        <?php 
+                $categories=  get_categories();
+                foreach ($categories as $cat) :
+                        $option = '<option value="' . $cat->cat_ID . '" ';
+                        if ($cat->cat_ID == $categoryid) :
+                                $option .= ' selected = "selected" ';
+                        endif;
+                        $option .=  '">';
+                        $option .= $cat->cat_name;
+                        $option .= '</option>';
+                        echo $option;
+                endforeach;
+        ?>
 </select></p>
 
-<p><label for="<?php echo $this->get_field_id('limit'); ?>"><?php _e("Number of posts")?></label>
+<p><label for="<?php echo $this->get_field_id('limit'); ?>"><?php _e("Number of posts", 'list-category-posts')?></label>
 <br/>
-	<input size="2" id="<?php echo $this->get_field_id('limit'); ?>" 
-	name="<?php echo $this->get_field_name('limit'); ?>" type="text" 
-	value="<?php echo attribute_escape($limit); ?>" />
+        <input size="2" id="<?php echo $this->get_field_id('limit'); ?>" 
+        name="<?php echo $this->get_field_name('limit'); ?>" type="text" 
+        value="<?php echo esc_attr($limit); ?>" />
 </p>
 
-<p><label for="<?php echo $this->get_field_id('offset'); ?>"><?php _e("Offset")?>: <br/>
-	<input size="2" id="<?php echo $this->get_field_id('offset'); ?>" 
-	name="<?php echo $this->get_field_name('offset'); ?>" type="text" 
-	value="<?php echo attribute_escape($offset); ?>" />
+<p><label for="<?php echo $this->get_field_id('offset'); ?>"><?php _e("Offset", 'list-category-posts')?>: <br/>
+        <input size="2" id="<?php echo $this->get_field_id('offset'); ?>" 
+        name="<?php echo $this->get_field_name('offset'); ?>" type="text" 
+        value="<?php echo esc_attr($offset); ?>" />
 </label></p>
 
-<p><label for="<?php echo $this->get_field_id('orderby'); ?>"><?php _e("Order by")?></label> <br/>
-	<select  id="<?php echo $this->get_field_id('orderby'); ?>" 
-		name="<?php echo $this->get_field_name('orderby'); ?>" type="text" >
-		<option value='date'>Date</option>
-		<option value='title'>Post title</option>
-		<option value='author'>Author</option>
-		<option value='rand'>Random</option>
-	</select>
+<p><label for="<?php echo $this->get_field_id('orderby'); ?>"><?php _e("Order by", 'list-category-posts')?></label> <br/>
+        <select  id="<?php echo $this->get_field_id('orderby'); ?>" 
+                name="<?php echo $this->get_field_name('orderby'); ?>" type="text" >
+                <option value='date'><?php _e("Date", 'list-category-posts')?></option>
+                <option value='title'><?php _e("Post title", 'list-category-posts')?></option>
+                <option value='author'><?php _e("Author", 'list-category-posts')?></option>
+                <option value='rand'><?php _e("Random", 'list-category-posts')?></option>
+        </select>
 </p>
 
-<p><label for="<?php echo $this->get_field_id('order'); ?>"><?php _e("Order")?></label><br/>
-	<select id="<?php echo $this->get_field_id('order'); ?>" 
-		name="<?php echo $this->get_field_name('order'); ?>" type="text">
-		<option value='desc'>Descending</option>
-		<option value='asc'>Ascending</option>
-	</select>
+<p><label for="<?php echo $this->get_field_id('order'); ?>"><?php _e("Order", 'list-category-posts')?></label><br/>
+        <select id="<?php echo $this->get_field_id('order'); ?>" 
+                name="<?php echo $this->get_field_name('order'); ?>" type="text">
+                <option value='desc'><?php _e("Descending", 'list-category-posts')?></option>
+                <option value='asc'><?php _e("Ascending", 'list-category-posts')?></option>
+        </select>
 </p>
 
-<p><label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e("Exclude categories (id's)")?></label><br/>
-	<input id="<?php echo $this->get_field_id('exclude'); ?>"
-	name="<?php echo $this->get_field_name('exclude'); ?>" type="text"
-	value="<?php echo attribute_escape($exclude); ?>" />
+<p><label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e("Exclude categories (id's)", 'list-category-posts')?></label><br/>
+        <input id="<?php echo $this->get_field_id('exclude'); ?>"
+        name="<?php echo $this->get_field_name('exclude'); ?>" type="text"
+        value="<?php echo esc_attr($exclude); ?>" />
 </p>
 
-<p><label for="<?php echo $this->get_field_id('excludeposts'); ?>"><?php _e("Exclude posts (id's)")?></label><br/>
-	<input id="<?php echo $this->get_field_id('excludeposts'); ?>"
-	name="<?php echo $this->get_field_name('excludeposts'); ?>" type="text"
-	value="<?php echo attribute_escape($excludeposts); ?>" />
+<p><label for="<?php echo $this->get_field_id('excludeposts'); ?>"><?php _e("Exclude posts (id's)", 'list-category-posts')?></label><br/>
+        <input id="<?php echo $this->get_field_id('excludeposts'); ?>"
+        name="<?php echo $this->get_field_name('excludeposts'); ?>" type="text"
+        value="<?php echo esc_attr($excludeposts); ?>" />
 </p>
 
 
-		<label>Show: </label><br/>
+                <label><?php _e("Show", 'list-category-posts')?>: </label><br/>
         <p>
             <input type="checkbox" <?php checked( (bool) $instance['thumbnail'], true ); ?>
-            name="<?php echo $this->get_field_name( 'thumbnail'); ?>" /><?php _e("Thumbnail - size")?> 
+            name="<?php echo $this->get_field_name( 'thumbnail'); ?>" /><?php _e("Thumbnail - size", 'list-category-posts')?> 
             <select id="<?php echo $this->get_field_id('thumbnail_size'); ?>"
                 name="<?php echo $this->get_field_name( 'thumbnail_size' ); ?>" type="text">
                 <option value='thumbnail'>thumbnail</option>
@@ -117,18 +117,17 @@ $thumbnail_size = strip_tags($instance['thumbnail_size']);
         </p>
         <p>
             <input type="checkbox" <?php checked( (bool) $instance['show_date'], true ); ?>
-            name="<?php echo $this->get_field_name( 'show_date' ); ?>" /><?php _e("Date")?>
+            name="<?php echo $this->get_field_name( 'show_date' ); ?>" /><?php _e("Date", 'list-category-posts')?>
         </p>
         <p>
             <input type="checkbox" <?php checked( (bool) $instance['show_author'], true ); ?>
-            name="<?php echo $this->get_field_name( 'show_author' ); ?>" /><?php _e("Author")?>
+            name="<?php echo $this->get_field_name( 'show_author' ); ?>" /><?php _e("Author", 'list-category-posts')?>
         </p>
         <p>
             <input type="checkbox" <?php checked( (bool) $instance['show_catlink'], true ); ?>
-            name="<?php echo $this->get_field_name( 'show_catlink' ); ?>" /><?php _e("Link to category")?>
+            name="<?php echo $this->get_field_name( 'show_catlink' ); ?>" /><?php _e("Link to category", 'list-category-posts')?>
         </p>
         <p>
             <input type="checkbox" <?php checked( (bool) $instance['show_excerpt'], true ); ?>
-            name="<?php echo $this->get_field_name( 'show_excerpt' ); ?>" /><?php _e("Excerpt")?>
+            name="<?php echo $this->get_field_name( 'show_excerpt' ); ?>" /><?php _e("Excerpt", 'list-category-posts')?>
         </p>
-
