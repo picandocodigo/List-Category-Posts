a93cdad7d43bda8e9c956b7671652e76f2932c92
Klemens Starybrat <klemens.starybrat@gmail.com>
2018-01-04 21:03:30 +0100

    Add tests for lcp_orders

---
+++ tests/lcputils/test-lcpOrders.php
@@ -0,0 +1,13 @@
+<?php
+
+class Tests_LcpUtils_LcpOrders extends WP_UnitTestCase {
+
+    public function test_if_arrays_equal() {
+        $lcp_order = array("date" => __("Date", "list-category-posts"),
+                           "modified" => __("Modified Date", "list-category-posts"),
+                           "title" => __("Post title", "list-category-posts"),
+                           "author" => __("Author", "list-category-posts"),
+                           "rand" => __("Random", "list-category-posts"));
+    $this->assertTrue( LcpUtils::lcp_orders() === $lcp_order);
+    }
+}
