406d85b70c7212fd85b3b83f8b94caf7ea1c6147
Fernando Briano <fernando@picandocodigo.net>
2020-06-15 19:52:47 +0100

    Refactors lcp-category

--- include/lcp-category.php
+++ include/lcp-category.php
@@ -18,44 +18,23 @@ class LcpCategory{
   /*
    * When the category is set using the `name` parameter.
    */
-  public function with_name($name){
-    $lcp_category_id = null;
-    // AND relationship
-    if ( preg_match('/\+/', $name) ){
-      $categories = array();
-      $cat_array = explode("+", $name);
-
-      foreach ($cat_array as $category){
-        $categories[] = $this->get_category_id_by_name($category);
-      }
-      $lcp_category_id = $categories;
-      // OR relationship
-    } elseif (preg_match('/,/', $name )){
-      $categories = '';
-      $cat_array = explode(",", $name);
-
-      foreach ($cat_array as $category){
-        $id = $this->get_category_id_by_name($category);
-        $categories .= $id . ",";
-      }
-
-      $lcp_category_id = $categories;
-    } else {
-      $lcp_category_id = $this->get_category_id_by_name($name);
+  public function withName($name){
+    if ( preg_match('/\+/', $name) ){ // AND relationship
+      return $this->andRelationship($name);
+    } elseif (preg_match('/,/', $name )){ // OR relationship
+      return $this->orRelationship($name);
     }
-    return $lcp_category_id;
+    return $this->getCategoryIdByName($name);
   }
 
-  public function with_id($id){
-    if (preg_match('/\+/', $id)){
-      if ( preg_match('/(-[0-9]+)+/', $id, $matches) ){
+  public function withId($catId){
+    if (preg_match('/\+/', $catId)){
+      if ( preg_match('/(-[0-9]+)+/', $catId, $matches) ){
         $this->exclude = implode(',', explode("-", ltrim($matches[0], '-') ));
       }
-      $lcp_category_id = array_map( 'intval', explode( "+", $id ) );
-    } else {
-      $lcp_category_id = $id;
+      return array_map( 'intval', explode( "+", $catId ) );
     }
-    return $lcp_category_id;
+    return $catId;
   }
 
   public function current_category($mode){
@@ -75,36 +54,58 @@ class LcpCategory{
         $categories = get_the_category($post->ID);
       }
       if ( !empty($categories) ){
-        $cats = array_map(function($cat) {
-          return $cat->cat_ID;
-        }, $categories);
-        // AND relationship
-        if ('all' === $mode) return $cats;
-        // OR relationship, default
-        if ('yes' === $mode || '' === $mode) return implode(',', $cats);
-        // Exclude current categories
-        if ('other' === $mode) return implode(',', array_map(function($cat) {
-          return "-$cat";
-        }, $cats));
-      } else {
         return [0]; // workaround to display no posts
       }
+      $cats = array_map(function($cat) {
+        return $cat->cat_ID;
+      }, $categories);
+
+      // AND relationship
+      if ('all' === $mode) return $cats;
+      // OR relationship, default
+      if ('yes' === $mode || '' === $mode) return implode(',', $cats);
+      // Exclude current categories
+      if ('other' === $mode) return implode(',', array_map(function($cat) {
+        return "-$cat";
+      }, $cats));
     }
     return $category->cat_ID;
   }
+
 
   /**
    * Get the category id from its name
    * by Eric Celeste / http://eric.clst.org
    */
-  private function get_category_id_by_name($cat_name){
+  private function getCategoryIdByName($categoryName){
     //TODO: Support multiple names (this used to work, but not anymore)
     //We check if the name gets the category id, if not, we check the slug.
-    $term = get_term_by('slug', $cat_name, 'category');
+    $term = get_term_by('slug', $categoryName, 'category');
     if (!$term){
-      $term = get_term_by('name', $cat_name, 'category');
+      $term = get_term_by('name', $categoryName, 'category');
     }
     return ($term) ? $term->term_id : 0;
   }
 
+  private function andRelationship($name){
+    $categories = array();
+    $catArray = explode("+", $name);
+
+    foreach ($catArray as $category){
+      $categories[] = $this->getCategoryIdByName($category);
+    }
+    return $categories;
+  }
+
+  private function orRelationship($name) {
+    $categories = '';
+    $catArray = explode(",", $name);
+
+    foreach ($catArray as $category){
+      $id = $this->getCategoryIdByName($category);
+      $categories .= $id . ",";
+    }
+
+    return $categories;
+  }
 }
