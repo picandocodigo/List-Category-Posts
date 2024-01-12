picandocodigo/List-Category-Posts

commit 2af44e364a83fafbd17f385c06267e5f57750bec

Implement WordPress style data escaping

include/lcp-paginator.php
@@ -112,7 +112,7 @@ class LcpPaginator {
     $link = '';
 
     if ($page == $current_page){
-      $link = "<li class='lcp_currentpage'>$current_page</li>";
+      $link = "<li class='lcp_currentpage'>" . esc_html($current_page) . '</li>';
     } else {
       $server_vars = add_magic_quotes($_SERVER);
       $request_uri = $server_vars['REQUEST_URI'];
@@ -134,14 +134,14 @@ class LcpPaginator {
       // Append a bookmark if not disabled by 'pagination_bookmarks=no'
       if ($bookmark !== "no") $page_link .= "#lcp_instance_" . $lcp_instance;
 
-      $link .=  "<li><a href='$page_link' title='$page'";
+      $link .=  "<li><a href='" . esc_url($page_link) . "' title='" . esc_attr($page) . "'";
       if ($page === $this->prev_page_num) {
         $link .= " class='lcp_prevlink'";
       } elseif ($page === $this->next_page_num) {
         $link .= " class='lcp_nextlink'";
       }
       $link .= ">";
-      ($char != null) ? ($link .= $char) : ($link .= $page);
+      ($char != null) ? ($link .= esc_html($char)) : ($link .= esc_html($page));
 
       $link .= "</a></li>";
     }

include/lcp-catlist.php
@@ -176,11 +176,13 @@ class CatList{
           $cat_string = $cat_title;
         }
 
+        $cat_string = esc_html($cat_string);
+
         // Do we want the link or just the title?
         if ($this->params['catlink'] == 'yes') {
           $cat_string = $this->wrapper->to_html(
             'a',
-            ['href' => $cat_link],
+            ['href' => esc_url($cat_link)],
             $cat_string . $this->get_category_count()
           );
         }
@@ -198,8 +200,8 @@ class CatList{
    */
   public function get_morelink() {
     if (!empty($this->params['morelink'])) {
-      $props = ['href' => get_category_link($this->lcp_category_id)];
-      $readmore = ($this->params['morelink'] !== '' ? $this->params['morelink'] : 'More posts');
+      $props = ['href' => esc_url(get_category_link($this->lcp_category_id))];
+      $readmore = ($this->params['morelink'] !== '' ? esc_html($this->params['morelink']) : 'More posts');
       return $this->wrapper->to_html('a', $props, $readmore);
      } else {
       return null;
@@ -208,31 +210,31 @@ class CatList{
 
   public function get_posts_morelink($single, $css_class) {
     if(!empty($this->params['posts_morelink'])) {
-      $props = ['href' => get_permalink($single->ID)];
+      $props = ['href' => esc_url(get_permalink($single->ID))];
       $class = $css_class ?: "";
       if ($class) {
-        $props['class'] = $class;
+        $props['class'] = esc_attr($class);
       }
-      $readmore = $this->params['posts_morelink'];
+      $readmore = esc_html($this->params['posts_morelink']);
       return $this->wrapper->to_html('a', $props, $readmore);
     }
   }
 
   public function get_category_count() {
     if($this->utils->lcp_not_empty('category_count') && $this->params['category_count'] == 'yes') {
-      return ' ' . get_category($this->lcp_category_id)->category_count;
+      return ' ' . esc_html(get_category($this->lcp_category_id)->category_count);
     }
   }
 
   public function get_category_description() {
     if ($this->utils->lcp_not_empty('category_description') && $this->params['category_description'] == 'yes') {
-      return get_term_field('description', $this->lcp_category_id, '', 'raw');
+      return esc_html(get_term_field('description', $this->lcp_category_id, '', 'raw'));
     }
   }
 
   public function get_conditional_title() {
     if($this->utils->lcp_not_empty('conditional_title') && $this->get_posts_count() > 0) {
-      return trim($this->params['conditional_title']);
+      return esc_html(trim($this->params['conditional_title']));
     }
   }
 
@@ -265,7 +267,7 @@ class CatList{
               $value = $user_customfield . $this->params['customfield_display_name_glue'] . $value;
             }
             if($value != '') {
-              $lcp_customs[] = $value;
+              $lcp_customs[] = wp_kses_post($value);
             }
           }
         }
@@ -286,7 +288,7 @@ class CatList{
   public function get_comments_count($single) {
     if (isset($this->params['comments']) &&
         $this->params['comments'] == 'yes') {
-      return ' (' . $single->comment_count . ')';
+      return esc_html(' (' . $single->comment_count . ')');
     } else {
       return null;
     }
@@ -309,7 +311,7 @@ class CatList{
     // Construct output string based on $params.
     $output = '';
     if (is_array($terms)) {
-      $output .= $params['prefix'];
+      $output .= esc_html($params['prefix']);
       $parsed_terms = array();
       foreach($terms as $term) {
         $term_string = $term->name;
@@ -345,22 +347,22 @@ class CatList{
     if ($link) {
       $term_string = $this->wrapper->to_html(
         'a',
-        ['href' => get_term_link($term->term_id)],
-        $term_string
+        ['href' => esc_url(get_term_link($term->term_id))],
+        esc_html($term_string)
       );
     }
     return $term_string;
   }
 
-  private function pt_inner_wrapper($inner, $tax, $term, $term_string) {
+  private function pt_inner_wrapper($inner, $tax, $term, $term_string_safe) {
     if ($inner) {
-      $term_string = $this->wrapper->to_html(
+      $term_string_safe = $this->wrapper->to_html(
         $inner,
-        ['class' => "{$tax}-{$term->slug}"],
-        $term_string
+        ['class' => esc_attr("{$tax}-{$term->slug}")],
+        $term_string_safe
       );
     }
-    return $term_string;
+    return $term_string_safe;
   }
 
   public function get_author_to_show($single) {
@@ -372,11 +374,11 @@ class CatList{
         $link = get_author_posts_url($lcp_userdata->ID);
         return $this->wrapper->to_html(
           'a',
-          ['href' => $link, 'title' => $author_name],
+          ['href' => esc_url($link), 'title' => esc_attr($author_name)],
           $author_name
         );
       } else {
-        return $author_name;
+        return esc_html($author_name);
       }
     } else {
       return null;
@@ -409,7 +411,7 @@ class CatList{
   public function get_date_to_show($single) {
     if ($this->params['date'] == 'yes') {
       //by Verex, great idea!
-      return ' ' . get_the_time($this->params['dateformat'], $single);
+      return ' ' . esc_html(get_the_time($this->params['dateformat'], $single));
     } else {
       return null;
     }
@@ -425,14 +427,14 @@ class CatList{
 
   public function get_display_id($single) {
     if (!empty($this->params['display_id']) && $this->params['display_id'] == 'yes') {
-      return $single->ID;
+      return esc_html($single->ID);
     }
   }
 
   public function get_no_posts_text() {
     if (($this->get_posts_count() == 0) &&
         ($this->params["no_posts_text"] != '')) {
-      return $this->params["no_posts_text"];
+      return esc_html($this->params["no_posts_text"]);
     }
   }
 
@@ -457,7 +459,7 @@ class CatList{
           $lcp_more = __('Continue reading &rarr;', 'list-category-posts');
           $lcp_content .= $this->wrapper->to_html(
             'a',
-            ['href' => get_permalink($single->ID), 'title' => $lcp_more],
+            ['href' => esc_url(get_permalink($single->ID)), 'title' => esc_attr($lcp_more)],
             $lcp_more
           );
         }
@@ -541,19 +543,19 @@ class CatList{
 
     $props = [];
     if ($tag == 'ol' && !empty($this->params['ol_offset'])) {
-      $props['start'] = $this->params['ol_offset'];
+      $props['start'] = esc_attr($this->params['ol_offset']);
     }
 
     // Follow the number of posts in an ordered list with pagination.
     if('ol' === $tag && $this->page > 1) {
       $start = $this->get_number_posts() * ($this->page - 1) + 1;
-      $props['start'] = $start;
+      $props['start'] = esc_attr($start);
     }
     //Give a class to wrapper tag
     $props['class'] = LcpUtils::sanitize_html_classes($css_class);
 
     //Give id to wrapper tag
-    $props['id'] = 'lcp_instance_' . $this->instance;
+    $props['id'] = esc_attr('lcp_instance_' . $this->instance);
 
     return $this->wrapper->to_html($tag, $props, null, false);
   }
@@ -570,12 +572,12 @@ class CatList{
       $post_tags = wp_get_post_Tags($single->ID);
       if (!empty($post_tags)) {
         foreach ($post_tags as $post_tag) {
-          $class .= " $post_tag->slug ";
+          $class .= esc_attr(" $post_tag->slug ");
         }
       }
     }
     if (!empty($class)) {
-      $props['class'] = $class;
+      $props['class'] = esc_attr($class);
     }
     return $this->wrapper->to_html($tag, $props, null, false);
   }

include/lcp-catlistdisplayer.php
@@ -60,7 +60,7 @@ class CatListDisplayer {
   }
 
   private function close_outer_tag() {
-    return '</' . $this->templater->outer_tag . '>';
+    return '</' . esc_attr($this->templater->outer_tag) . '>';
   }
 
   public function get_morelink($tag = null, $css_class = null){
@@ -104,7 +104,7 @@ class CatListDisplayer {
   }
 
   private function close_inner_tag() {
-    return '</' . $this->templater->inner_tag . '>';
+    return '</' . esc_attr($this->templater->inner_tag) . '>';
   }
 
   private function get_comments($single, $tag = null, $css_class = null){
@@ -231,14 +231,14 @@ class CatListDisplayer {
 
   private function get_post_link($single, $text, $class = null){
 
-    $props = ['href' => get_permalink($single->ID)];
+    $props = ['href' => esc_url(get_permalink($single->ID))];
 
     if (!empty($class)) {
-      $props['class'] = $class;
+      $props['class'] = esc_attr($class);
     }
 
     if (!empty($this->params['link_target'])) {
-      $props['target'] = $this->params['link_target'];
+      $props['target'] = esc_attr($this->params['link_target']);
     }
 
     $output = $this->wrapper->to_html('a', $props, $text);
@@ -261,7 +261,7 @@ class CatListDisplayer {
     }
 
     // Shortcode parameters take precedence.
-    $tag = $this->params['title_tag'] ?: $tag;
+    $tag = $this->params['title_tag'] ?: esc_attr($tag);
     $css_class = $this->params['title_class'] ?: $css_class;
     $suffix = $this->params['post_suffix'] ? ' ' . $this->params['post_suffix'] : '';
 
@@ -280,11 +280,11 @@ class CatListDisplayer {
 
     if (!$link) {
       $post_title .= $suffix;
-      $output = $this->wrapper->wrap($post_title, $tag, $css_class);
+      $output = $this->wrapper->wrap(wp_kses_data($post_title), $tag, $css_class);
     } else if (empty($tag)) {
-      $output = $this->get_post_link($single, $post_title, $css_class) . $suffix;
+      $output = $this->get_post_link($single, wp_kses_data($post_title), $css_class) . esc_html($suffix);
     } else if (!empty($tag)) {
-      $output = $this->get_post_link($single, $post_title) . $suffix;
+      $output = $this->get_post_link($single, wp_kses_data($post_title)) . esc_html($suffix);
       $output = $this->wrapper->wrap($output, $tag, $css_class);
     }
 

include/lcp-thumbnail.php
@@ -47,7 +47,7 @@ class LcpThumbnail{
             array(
                 'alt' => esc_attr($single->post_title),
                 // If we have a class use it, otherwise use default
-                'class' => ( $lcp_thumb_class != null ) ? $lcp_thumb_class : 'lcp_thumbnail'
+                'class' => ( $lcp_thumb_class != null ) ? esc_attr($lcp_thumb_class) : 'lcp_thumbnail'
             )
         );
         $lcp_thumbnail .= '</a>';

include/lcp-wrapper.php
@@ -72,7 +72,8 @@ class LcpWrapper {
    * Wraps $content in HTML tag specified in $tag. If $close is false
    * the closing tag is omitted (useful when $content is null and we only
    * want to open the tag). $properties is an associative array with HTML
-   * properties.
+   * properties. Tag can be passed unescaped, all other parameters
+   * are expected to be safe.
    *
    * @param  string  $tag        HTML tag (ex. 'p', 'div', 'li').
    * @param  array   $properties Optional. HTML element properties in
@@ -87,8 +88,8 @@ class LcpWrapper {
       $props_str .= ' ' . $property . '="' . $value . '"';
     }
 
-    $html = "<{$tag}" . $props_str . ">{$content}";
-    if ($close) $html .= "</{$tag}>";
+    $html = '<' . esc_attr($tag) . $props_str . '>' . $content;
+    if ($close) $html .= '</' . esc_attr($tag) . '>';
 
     return $html;
   }

tests/lcppaginator/test-getPagination.php
@@ -36,7 +36,7 @@ class Tests_LcpPaginator_GetPagination extends WP_UnitTestCase {
                   "<li class='lcp_currentpage'>1</li>" .
                   "<li><a href='http://example.org/?lcp_page0=2#lcp_instance_0' title='2'>2</a></li>" .
                   "<li><a href='http://example.org/?lcp_page0=3#lcp_instance_0' title='3'>3</a></li>" .
-                  "<li><a href='http://example.org/?lcp_page0=2#lcp_instance_0' title='2' class='lcp_nextlink'>>></a></li>" .
+                  "<li><a href='http://example.org/?lcp_page0=2#lcp_instance_0' title='2' class='lcp_nextlink'>&gt;&gt;</a></li>" .
                   "</ul>";
     $this->assertSame($exp_string, $pag_string);
 
@@ -49,11 +49,11 @@ class Tests_LcpPaginator_GetPagination extends WP_UnitTestCase {
     $pag_string = $paginator->get_pagination($params);
 
     $exp_string = "<ul class='lcp_paginator'>" .
-                  "<li><a href='http://example.org/?lcp_page0=1#lcp_instance_0' title='1' class='lcp_prevlink'><<</a></li>" .
+                  "<li><a href='http://example.org/?lcp_page0=1#lcp_instance_0' title='1' class='lcp_prevlink'>&lt;&lt;</a></li>" .
                   "<li><a href='http://example.org/?lcp_page0=1#lcp_instance_0' title='1'>1</a></li>" .
                   "<li class='lcp_currentpage'>2</li>" .
                   "<li><a href='http://example.org/?lcp_page0=3#lcp_instance_0' title='3'>3</a></li>" .
-                  "<li><a href='http://example.org/?lcp_page0=3#lcp_instance_0' title='3' class='lcp_nextlink'>>></a></li>" .
+                  "<li><a href='http://example.org/?lcp_page0=3#lcp_instance_0' title='3' class='lcp_nextlink'>&gt;&gt;</a></li>" .
                   "</ul>";
     $this->assertSame($exp_string, $pag_string);
 
@@ -66,7 +66,7 @@ class Tests_LcpPaginator_GetPagination extends WP_UnitTestCase {
     $pag_string = $paginator->get_pagination($params);
 
     $exp_string = "<ul class='lcp_paginator'>" .
-                  "<li><a href='http://example.org/?lcp_page0=2#lcp_instance_0' title='2' class='lcp_prevlink'><<</a></li>" .
+                  "<li><a href='http://example.org/?lcp_page0=2#lcp_instance_0' title='2' class='lcp_prevlink'>&lt;&lt;</a></li>" .
                   "<li><a href='http://example.org/?lcp_page0=1#lcp_instance_0' title='1'>1</a></li>" .
                   "<li><a href='http://example.org/?lcp_page0=2#lcp_instance_0' title='2'>2</a></li>" .
                   "<li class='lcp_currentpage'>3</li>" .
@@ -88,7 +88,7 @@ class Tests_LcpPaginator_GetPagination extends WP_UnitTestCase {
                   "<li class='lcp_currentpage'>1</li>" .
                   "<li><a href='http://example.org/?lcp_page0=2' title='2'>2</a></li>" .
                   "<li><a href='http://example.org/?lcp_page0=3' title='3'>3</a></li>" .
-                  "<li><a href='http://example.org/?lcp_page0=2' title='2' class='lcp_nextlink'>>></a></li>" .
+                  "<li><a href='http://example.org/?lcp_page0=2' title='2' class='lcp_nextlink'>&gt;&gt;</a></li>" .
                   "</ul>";
     $this->assertSame($exp_string, $pag_string);
   }
@@ -114,7 +114,7 @@ class Tests_LcpPaginator_GetPagination extends WP_UnitTestCase {
                   "<li><a href='http://example.org/?lcp_page0=6#lcp_instance_0' title='6'>6</a></li>" .
                   "<span class='lcp_elipsis'>...</span>" .
                   "<li><a href='http://example.org/?lcp_page0=20#lcp_instance_0' title='20'>20</a></li>" .
-                  "<li><a href='http://example.org/?lcp_page0=2#lcp_instance_0' title='2' class='lcp_nextlink'>>></a></li>" .
+                  "<li><a href='http://example.org/?lcp_page0=2#lcp_instance_0' title='2' class='lcp_nextlink'>&gt;&gt;</a></li>" .
                   "</ul>";
 
     $this->assertSame( $exp_string, $pag_string );
@@ -128,7 +128,7 @@ class Tests_LcpPaginator_GetPagination extends WP_UnitTestCase {
     $pag_string = $paginator->get_pagination( $params );
 
     $exp_string = "<ul class='lcp_paginator'>" .
-                  "<li><a href='http://example.org/?lcp_page0=9#lcp_instance_0' title='9' class='lcp_prevlink'><<</a></li>" .
+                  "<li><a href='http://example.org/?lcp_page0=9#lcp_instance_0' title='9' class='lcp_prevlink'>&lt;&lt;</a></li>" .
                   "<li><a href='http://example.org/?lcp_page0=1#lcp_instance_0' title='1'>1</a></li>" .
                   "<span class='lcp_elipsis'>...</span>" .
                   "<li><a href='http://example.org/?lcp_page0=5#lcp_instance_0' title='5'>5</a></li>" .
@@ -144,7 +144,7 @@ class Tests_LcpPaginator_GetPagination extends WP_UnitTestCase {
                   "<li><a href='http://example.org/?lcp_page0=15#lcp_instance_0' title='15'>15</a></li>" .
                   "<span class='lcp_elipsis'>...</span>" .
                   "<li><a href='http://example.org/?lcp_page0=20#lcp_instance_0' title='20'>20</a></li>" .
-                  "<li><a href='http://example.org/?lcp_page0=11#lcp_instance_0' title='11' class='lcp_nextlink'>>></a></li>" .
+                  "<li><a href='http://example.org/?lcp_page0=11#lcp_instance_0' title='11' class='lcp_nextlink'>&gt;&gt;</a></li>" .
                   "</ul>";
 
     $this->assertSame( $exp_string, $pag_string );
@@ -157,7 +157,7 @@ class Tests_LcpPaginator_GetPagination extends WP_UnitTestCase {
     $pag_string = $paginator->get_pagination( $params );
 
     $exp_string = "<ul class='lcp_paginator'>" .
-                  "<li><a href='http://example.org/?lcp_page0=19#lcp_instance_0' title='19' class='lcp_prevlink'><<</a></li>" .
+                  "<li><a href='http://example.org/?lcp_page0=19#lcp_instance_0' title='19' class='lcp_prevlink'>&lt;&lt;</a></li>" .
                   "<li><a href='http://example.org/?lcp_page0=1#lcp_instance_0' title='1'>1</a></li>" .
                   "<span class='lcp_elipsis'>...</span>" .
                   "<li><a href='http://example.org/?lcp_page0=15#lcp_instance_0' title='15'>15</a></li>" .

commit aff2d4231029fef20c795110276b56c5f41a4920

Fix pagination links
Security improvements in 2af44e3 introduced a bug in pageination
links when pagination is the first query var in the query string.

Fixes #511

include/lcp-paginator.php
@@ -134,6 +134,9 @@ class LcpPaginator {
       // Append a bookmark if not disabled by 'pagination_bookmarks=no'
       if ($bookmark !== "no") $page_link .= "#lcp_instance_" . $lcp_instance;
 
+      // WA: Replace '?&' by '?' to avoid potential redirection problems later on
+      $page_link = str_replace('?&', '?', $page_link );
+
       $link .=  "<li><a href='" . esc_url($page_link) . "' title='" . esc_attr($page) . "'";
       if ($page === $this->prev_page_num) {
         $link .= " class='lcp_prevlink'";
@@ -145,8 +148,6 @@ class LcpPaginator {
 
       $link .= "</a></li>";
     }
-    // WA: Replace '?&' by '?' to avoid potential redirection problems later on
-    $link = str_replace('?&', '?', $link );
     return $link;
   }
 }